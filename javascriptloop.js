0aa69cf7579971fd5a2bd53f1e105710b9b1ee8b
abc@mail.com <abc@mail.com>
2023-10-19 16:55:52 -0500

    Loop to show character occurance

--- javascriptloop.js
+++ javascriptloop.js
@@ -1,38 +1,29 @@
-/*for(let i=1;i<=5;i++){
-    for(let j=1;j<=i;j++){
-
-        process.stdout.write("*")
-    }
-    console.log("")
-}
-for(var z = 1; z<10; z++){
-    if(z==5){
-        continue
-    }
-    console.log(z)
-}*/
-// program to check the number of occurrence of a character..
-
-
-const str ="Hi there, ! How is your day today ?";
-const letter ='H';
-function countString(str, letter) {
-    
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i).toLowerCase() == letter.toLowerCase()) {
-            count += 1;
+      
+       
+      
+    function count( str )
+    {
+        for( let i = 0 ;i < str.length ;i++)
+        { 
+            let count = 0;
+            for( let j = 0 ;j < str.length ;j++)
+            {
+                if( str[i].toUpperCase() == str[j].toUpperCase() && i > j  )
+                    {
+                    break;
+                    }
+                if( str[i].toUpperCase() == str[j].toUpperCase() )
+                    {
+                    count++;
+                    }
+                }
+                if( count > 0)
+                console.log(str[i].toUpperCase()+"====>"+count );
         }
+            
     }
-    return count;
-}
-const result = countString(str, letter);
-console.log(str);
-console.log("You selected the Letter :  " +letter);
-console.log("The  letter "+letter   +" occurs "  +result+  " times in the string");
-
-
-
-
-
-
+        let strng="Hey there ! how are you today?\n";
+        console.log("\nYour String:\n"+strng)
+        str =strng.split(" ").join("");
+        count( str);
+        
