47c7f3c611d4f8e23ad0f313e52b5f33f3ebaf8b
abc@mail.com <abc@mail.com>
2023-10-28 16:09:45 -0500

    task -6 changes

--- task6,9/test.js
+++ task6,9/test.js
@@ -1,34 +1,75 @@
-function send(){    
 
-    let details={
-    // Name:document.getElementById("txtnum1").value,
-    // Employee_id:document.getElementById("empid").value,
-    // Joining_date:document.getElementById("doj").value,
-    // Designation:document.getElementById("pob").value,
-    // Hours_worked:document.getElementById("hours").value
-    Name:$("#txtnum1").val(),
-    Employee_id:$("#empid").val(),
-    Joining_date:$("#doj").val(),
-    Designation:$("#pob").val(),
-    Hours_worked:$("#hours").val()
+// //USING FUNCTION
+// //========================================
+// function send(){    
+
+//     let details={
+//     // Name:document.getElementById("txtnum1").value,
+//     // Employee_id:document.getElementById("empid").value,
+//     // Joining_date:document.getElementById("doj").value,
+//     // Designation:document.getElementById("pob").value,
+//     // Hours_worked:document.getElementById("hours").value
+//     Name:$("#txtnum1").val(),
+//     Employee_id:$("#empid").val(),
+//     Joining_date:$("#doj").val(),
+//     Designation:$("#pob").val(),
+//     Hours_worked:$("#hours").val()
 
     
-}
-let salary
-if (details.Designation=="Manager") {
-    salary = details.Hours_worked*90;
-} else if(details.Designation=="Consultant") {
-    salary= details.Hours_worked*70;
-}
-else if(details.Designation=="Trainee".localeCompare){
-    salary =details.Hours_worked*45;
-}
-alert("hello world")
-//use this if you  want to get all the details which we give in form 
-//localStorage.setItem("itemvalue", JSON.stringify(details))
-let textValue ="Hello   "+ details.Name +"    "+"you  are  a  awesome    "+"    "+details.Designation +"   "+ "    and  proudly  get  a  salary  of  $ "+ salary;
-localStorage.setItem("itemvalue", JSON.stringify(textValue))
+// }
+// let salary
+// if (details.Designation=="Manager") {
+//     salary = details.Hours_worked*90;
+// } else if(details.Designation=="Consultant") {
+//     salary= details.Hours_worked*70;
+// }
+// else if(details.Designation=="Trainee".localeCompare){
+//     salary =details.Hours_worked*45;
+// }
+// alert("hello world")
+// //use this if you  want to get all the details which we give in form 
+// //localStorage.setItem("itemvalue", JSON.stringify(details))
+// let textValue ="Hello   "+ details.Name +"    "+"you  are  a  awesome    "+"    "+details.Designation +"   "+ "    and  proudly  get  a  salary  of  $ "+ salary;
+// localStorage.setItem("itemvalue", JSON.stringify(textValue))
 
+// }
+// // console.log("===========================================================================================")
+
+
+
+
+//USING CLASS
+//=================================================
+
+class Employee {
+    constructor(name, design, workhrs,joiningDate,empid,sal) {
+    this.name = name;
+    this.design = design;
+    this.workhrs = workhrs;
+    this.joiningDate=joiningDate;
+    this.empid = empid
+    this.sal =sal
+    }
 }
-console.log("===========================================================================================")
+function send()
+{
+    let name=$("#txtnum1").val();
+    let empid=$("#empid").val();
+    let joiningDate=$("#doj").val();
+    let design=$("#pob").val();
+    let workhrs=$("#hours").val();
+    let sal;
+    if (design === "Manager" ){
+        sal= 90*workhrs;
+    }
+    else if (design === "Consultant"){
+        sal=75*workhrs;
+    }  
+    else if(design === "Trainer"){
+        sal=50*workhrs;
+    } 
+    let newEmployee = new Employee(name,design,workhrs,joiningDate,empid,sal);
+    alert("Data Send")
+    localStorage.setItem("itemvalue",JSON.stringify(newEmployee));
+}
 
--- task6,9/test1.js
+++ task6,9/test1.js
@@ -1,3 +1,28 @@
-let result =localStorage.getItem((("itemvalue")));
-document.getElementById("container").innerHTML = result;
-//$("div").html(result)
+
+
+//USING FUNCTION
+//======================================
+// let result =localStorage.getItem((("itemvalue")));
+// // document.getElementById("container").innerHTML = result;
+
+//USING JQUERY--for line 6
+//==========================
+// let result =localStorage.getItem("itemvalue")
+// $(document).ready(function(){
+//     alert("click ok to see your salary")
+//     $("#container").html(result)
+// })
+
+
+//FOR CLASS 
+//=====================================
+let res =localStorage.getItem("itemvalue");
+let res1=JSON.parse(res)
+//document.getElementById("container").innerHTML ="Hello   " res1.name+"   You are a awasome    "+res1.design+ "  and proudly getting salary of $  "+res1.sal;
+$(document).ready(function(){
+        alert("click ok to see your salary")
+        $("#container").html("Hello   "+ res1.name+"   You are a awasome    "+res1.design+ "  and proudly getting salary of $  "+res1.sal)
+    })
+
+
+
